66179004dcc52f906ebcc4b532eaaa770c9f7d87
Sunny Sharma <work77sharmasunny@gmail.com>
2025-03-21 10:20:41 +0530

    Update script.js

--- Number Color Coder/script.js
+++ Number Color Coder/script.js
@@ -1,1 +1,29 @@
+const container = document.getElementById('numberContainer');
 
+    
+    function isPrime(num) {
+      if (num <= 1) return false;
+      if (num === 2) return true;
+      for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+      }
+      return true;
+    }
+
+
+    for (let i = 1; i <= 100; i++) {
+      const numberDiv = document.createElement('div');
+      numberDiv.classList.add('number');
+      numberDiv.textContent = i;
+
+      if (isPrime(i)) {
+        numberDiv.style.backgroundColor = 'red';
+      } else if (i % 2 === 0) {
+        numberDiv.style.backgroundColor = 'green';
+      } else {
+        numberDiv.style.backgroundColor = 'yellow';
+        numberDiv.style.color = '#000'; 
+      }
+
+      container.appendChild(numberDiv);
+    }
